85f05b0901885f3e2d067accc35cf09e574b5d96
mriduldas07 <mriduldas0325@gmail.com>
2024-08-03 02:52:07 +0600

    navbar done

--- src/components/customs/Navbar.tsx
+++ src/components/customs/Navbar.tsx
@@ -1,3 +1,6 @@
+import { Phone, ShoppingBasket } from "lucide-react";
+import Link from "next/link";
+import { Button } from "../ui/button";
 import {
   Select,
   SelectContent,
@@ -9,8 +12,8 @@ import {
 export default function Navbar() {
   return (
     <header className="bg-white">
-      <nav className="container py-5">
-        <div className="flex items-center space-x-4">
+      <nav className="container py-5 flex items-center justify-between">
+        <div className="flex items-center gap-4">
           <span>Pizza</span>
           <Select>
             <SelectTrigger className="w-[180px] focus:ring-0">
@@ -19,9 +22,37 @@ export default function Navbar() {
             <SelectContent>
               <SelectItem value="cheesy-delight">Cheesy Delight</SelectItem>
               <SelectItem value="pizza-hut">Pizza hut</SelectItem>
-              <SelectItem value="system">System</SelectItem>
             </SelectContent>
           </Select>
+        </div>
+        <div className="flex items-center gap-4">
+          <ul className="flex items-center font-bold space-x-4">
+            <li>
+              <Link className="hover:text-primary" href={`/`}>
+                Menu
+              </Link>
+            </li>
+            <li>
+              <Link className="hover:text-primary" href={`/`}>
+                Orders
+              </Link>
+            </li>
+          </ul>
+          <div className="relative">
+            <Link href={"/cart"} className="hover:text-primary">
+              <ShoppingBasket />
+            </Link>
+            <span className="absolute -top-4 -right-5 h-6 w-6 flex justify-center items-center rounded-full bg-primary font-bold text-white">
+              3
+            </span>
+          </div>
+          <div className="flex items-center pl-12 gap-1">
+            <Phone />
+            <span>+8801571124907</span>
+          </div>
+          <div className="">
+            <Button size={"sm"}>Logout</Button>
+          </div>
         </div>
       </nav>
     </header>
